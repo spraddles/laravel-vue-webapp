884a3f5c50f938f1e6ad5e3592440f31dd9bd958
Brett Spradbury <brett.spradbury@bigpicturemedical.com>
2022-05-18 23:49:55 +1000

    update template to script setup

--- src/views/UserProfilePage.vue
+++ src/views/UserProfilePage.vue
@@ -1,58 +1,56 @@
+<script setup>
+import BaseLayout from '@/layouts/BaseLayout.vue';
+import BaseInput from '@/components/BaseInput.vue';
+import BaseButton from '@/components/BaseButton.vue';
+import BaseSelect from '@/components/BaseSelect.vue';
+import { onMounted, ref } from 'vue';
+
+const language = ref('');
+const languageOptions = ref([]);
+
+onMounted(() => {
+  getExistingLanguage();
+  setLanguage();
+  getLanguageOptions();
+});
+
+const getExistingLanguage = (() => {
+  // some API request that fetches existing language
+  // returns a string (if set) else returns empty string (if none set)
+  var a = 'Spanish';
+  language.value = a;
+});
+
+const setLanguage = (() => {
+  if(language.value === '') {
+    return 'Please select a language';
+  }
+  return language.value;
+});
+
+const getLanguageOptions = (() => {
+  // some API that fetches language options
+  var b = ['English', 'German', 'Spanish', 'Russian', 'Portuguese', 'French'];
+  languageOptions.value = b;
+});
+</script>
+
 <template>
   <BaseLayout>
     <div class="user-profile-page">
       <h1 class="mb-10">Welome to your profile page</h1>
       <form>
-        <BaseInput
-          name="firstName"
-          type="text"
-          label="First name"
-        />
-        <BaseInput
-          name="lastName"
-          type="text"
-          label="Last name"
-        />
+        <BaseInput name="firstName" type="text" label="First name" />
+        <BaseInput name="lastName" type="text" label="Last name" />
         <BaseSelect
-          :options="options"
+          v-model="language"
+          :options="languageOptions"
           label="Language"
         />
-        <BaseInput
-          v-model="language"
-          name="username"
-          type="email"
-          label="Username"
-        />
-        <BaseInput
-          name="password"
-          type="password"
-          label="Password"
-        />
+        <BaseInput name="username" type="email" label="Username" />
+        <BaseInput name="password" type="password" label="Password" />
         <BaseButton class="xl:w-96">Save</BaseButton>
       </form>
     </div>
   </BaseLayout>
 </template>
-
-<script>
-import BaseLayout from '@/layouts/BaseLayout.vue';
-import BaseInput from '@/components/BaseInput.vue';
-import BaseButton from '@/components/BaseButton.vue';
-import BaseSelect from '@/components/BaseSelect.vue';
-
-export default {
-  name: 'UserProfilePage',
-  components: {
-    BaseLayout,
-    BaseInput,
-    BaseButton,
-    BaseSelect
-  },
-  data() {
-    return {
-      langauge: '',
-      options: ['English', 'German', 'Spanish', 'Russian']
-    }
-  }
-};
-</script>
