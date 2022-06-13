a24b155c8b306500289a91f38d6000514ce479c5
Brett Spradbury <brett.spradbury@bigpicturemedical.com>
2022-06-13 13:32:22 +1000

    change vue desktop location

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,19 @@
+const path = require('path');
+
 module.exports = {
+  outputDir: 'desktop/dist',
   configureWebpack: {
     plugins: [],
     entry: {
-      app: './src/main.js',
+      app: './desktop/main.js',
     },
   },
+  chainWebpack: (config) => {
+    config.resolve.alias.set('@', path.resolve(__dirname, 'desktop'));
+    config.resolve.alias.set('@components', path.resolve(__dirname, 'desktop/components'));
+    config.resolve.alias.set('@layouts', path.resolve(__dirname, 'desktop/layouts'));
+    config.resolve.alias.set('@pages', path.resolve(__dirname, 'desktop/views'));
+    config.resolve.symlinks(false);
+    config.resolve.alias.set('vue', path.resolve('./node_modules/vue'));
+  }
 };
