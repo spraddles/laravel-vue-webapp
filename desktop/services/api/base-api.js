2b5359c02b296a7e75f7cf397ed4dfcce5f033da
Brett Spradbury <brett.spradbury@bigpicturemedical.com>
2022-07-19 21:39:21 +1000

    add base api

---
+++ desktop/services/api/base-api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const baseApi = axios.create({
+  baseURL: process.env.VUE_APP_LARAVEL_API_URI,
+  withCredentials: true,
+  timeout: 15000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+export default baseApi;
