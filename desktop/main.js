951d9d5e1f153e98fcdb4bba8b4953bf13e2007b
Brett Spradbury <brett.spradbury@bigpicturemedical.com>
2022-07-24 09:36:57 +1000

    install pinia

--- desktop/main.js
+++ desktop/main.js
@@ -1,10 +1,17 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { beforeEach } from '@router/middleware'
 import './index.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+const store = createPinia()
+
+app
+  .use(router)
+  .use(store)
+  .mount('#app')
 
 router.beforeResolve(
   (routeTo, routeFrom, next) =>
