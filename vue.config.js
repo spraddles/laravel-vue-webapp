fad8d2bb25750810cda8acd8255d14f2531c49f6
Brett Spradbury <brett.spradbury@bigpicturemedical.com>
2022-07-27 09:38:39 +1000

    remove redundant aliases

--- vue.config.js
+++ vue.config.js
@@ -13,11 +13,6 @@ module.exports = {
   },
   chainWebpack: (config) => {
     config.resolve.alias.set('@', path.resolve(__dirname, 'desktop'))
-    config.resolve.alias.set('@services', path.resolve(__dirname, 'desktop/services'))
-    config.resolve.alias.set('@router', path.resolve(__dirname, 'desktop/router'))
-    config.resolve.alias.set('@components', path.resolve(__dirname, 'desktop/components'))
-    config.resolve.alias.set('@layouts', path.resolve(__dirname, 'desktop/layouts'))
-    config.resolve.alias.set('@pages', path.resolve(__dirname, 'desktop/views'))
     config.resolve.symlinks(false)
     config.resolve.alias.set('vue', path.resolve('./node_modules/vue'))
   }
