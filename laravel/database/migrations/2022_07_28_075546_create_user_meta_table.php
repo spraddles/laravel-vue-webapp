18b99f6384a3ae91cadc2e3d04492aee5f8fb3ac
Brett Spradbury <brett.spradbury@bigpicturemedical.com>
2022-07-29 14:37:22 +1000

    add user_meta table migration

---
+++ laravel/database/migrations/2022_07_28_075546_create_user_meta_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_meta', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('date_of_birth')->nullable();
+            $table->string('favourite_food')->nullable();
+            $table->string('language')->nullable();
+            $table->string('gender')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_meta');
+    }
+};
