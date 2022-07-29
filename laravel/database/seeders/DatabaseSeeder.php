740dcf6133eede4aed42579f62d60cee7f26cb04
Brett Spradbury <brett.spradbury@bigpicturemedical.com>
2022-07-29 14:41:25 +1000

    update seeder

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\UserMeta;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,20 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
 
-        \App\Models\User::create([
-            'name' => 'Test',
+        User::factory(10)->has(UserMeta::factory())->create();
+
+        User::factory(1)->has(UserMeta::factory())->create([
             'email' => 'test@test.com',
             'password' => bcrypt('test'),
             'email_verified_at' => '2021-08-08 00:35:32'
         ]);
-
-        \App\Models\User::create([
-            'name' => 'Brett',
-            'email' => 'badsprad@hotmail.com',
-            'password' => bcrypt('test'),
-            'email_verified_at' => '2021-08-08 00:35:32'
-        ]);
+            
     }
 }
