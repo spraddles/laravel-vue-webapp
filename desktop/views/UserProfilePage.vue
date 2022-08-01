297d20c0461f0b8dbda0da3a85df2af9001cb0fc
Brett Spradbury <brett.spradbury@bigpicturemedical.com>
2022-08-02 09:13:17 +1000

    make inputs reactive

--- desktop/views/UserProfilePage.vue
+++ desktop/views/UserProfilePage.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
+import { userStore } from '@/store'
+import { meService } from '@/services'
 import BaseLayout from '@/layouts/BaseLayout.vue'
 import BaseInput from '@/components/BaseInput.vue'
 import BaseButton from '@/components/BaseButton.vue'
@@ -8,78 +10,43 @@ import BaseRadio from '@/components/BaseRadio.vue'
 import BaseCheckboxMulti from '@/components/BaseCheckboxMulti.vue'
 import BaseDatePicker from '@/components/BaseDatePicker.vue'
 
-const form = {
-  firstName: ref(''),
-  lastName: ref(''),
-  dateOfBirth: ref(''),
-  language: ref(''),
-  gender: ref(''),
-}
+const store = userStore()
 
+const firstName = ref('')
+const lastName = ref('')
+const dateOfBirth = ref('')
 const favouriteFood = ref([])
-const favouriteFoodOptions = ref([])
-const languageOptions = ref([])
-const genderOptions = ref([])
+const language = ref('')
+const gender = ref('')
+
+const favouriteFoodOptions = ref(['Thai', 'Italian', 'Mexican', 'Greek', 'Seafood', 'Steak', 'Chinese'])
+const languageOptions = ref(['English', 'German', 'Spanish', 'Russian', 'Portuguese', 'French', 'Italian'])
+const genderOptions = ref(['Male', 'Female', 'Other'])
 
 onMounted(() => {
-  getExistingFirstName()
-  getExistingLastName()
-  getExistingDateOfBirth()
-  getExistingFavouriteFood()
-  getFavouriteFoodOptions()
-  getExistingLanguage()
-  getLanguageOptions()
-  getExistingGender()
-  getGenderOptions()
+  getCurrentUserDetails()
 })
 
-// FIRST NAME
-const getExistingFirstName = (() => {
-  var a = 'John'
-  form.firstName.value = a
-})
 
-// LAST NAME
-const getExistingLastName = (() => {
-  var a = 'Smith'
-  form.lastName.value = a
-})
+const getCurrentUserDetails = async () => {
+  try {
 
-// DATE OF BIRTH
-const getExistingDateOfBirth = (() => {
-  var a = '01/01/1993'
-  form.dateOfBirth.value = a
-})
+    const initResponse = await meService.getDetails({ email: store.email })
+    const response = initResponse.data.data
 
-// FAVOURITE FOOD
-const getExistingFavouriteFood = (() => {
-  var a = ['Italian', 'Mexican']
-  favouriteFood.value = a
-})
-const getFavouriteFoodOptions = (() => {
-  var a = ['Thai', 'Italian', 'Mexican', 'Greek', 'Seafood', 'Steak']
-  favouriteFoodOptions.value = a
-})
+    firstName.value = response.first_name
+    lastName.value = response.last_name
+    dateOfBirth.value = response.date_of_birth
+    favouriteFood.value = response.favourite_food.split(',') // convert comma separated strings to array
+    language.value = response.language
+    gender.value = response.gender
 
-// LANGUAGE
-const getExistingLanguage = (() => {
-  var a = 'German'
-  form.language.value = a
-})
-const getLanguageOptions = (() => {
-  var a = ['English', 'German', 'Spanish', 'Russian', 'Portuguese', 'French']
-  languageOptions.value = a
-})
-
-// GENDER
-const getExistingGender = (() => {
-  var a = 'Female'
-  form.gender.value = a
-})
-const getGenderOptions = (() => {
-  var a = ['Male', 'Female', 'Other']
-  genderOptions.value = a
-})
+  } catch(error) {
+    if (error.response === 401) {
+      console.warn('Error retrieving data')
+    }
+  }
+}
 
 </script>
 
@@ -89,21 +56,21 @@ const getGenderOptions = (() => {
       <h1 class="mb-10">This is your profile page</h1>
       <form>
         <BaseInput 
-          v-model="form.firstName"
+          v-model="firstName"
           :name="'first_name'"
           type="text"
           :label="'First name'"
           :placeholder="'Type your first name'"
         />
         <BaseInput 
-          v-model="form.lastName"
+          v-model="lastName"
           :name="'last_name'"
           type="text"
           :label="'Last name'"
           :placeholder="'Type your last name'"
         />
         <BaseDatePicker
-          v-model="form.dateOfBirth"
+          v-model="dateOfBirth"
           :name="'date_of_birth'"
           :label="'Date of birth'"
         />
@@ -114,13 +81,13 @@ const getGenderOptions = (() => {
           :name="'favourite_food'"
         />
         <BaseSelect
-          v-model="form.language"
+          v-model="language"
           :options="languageOptions"
           :placeholder="'Please select a language'"
           :label="'Language'"
         />
         <BaseRadio
-          v-model="form.gender"
+          v-model="gender"
           :options="genderOptions"
           :heading="'Gender'"
           :name="'gender'"
